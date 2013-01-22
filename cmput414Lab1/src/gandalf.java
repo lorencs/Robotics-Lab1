398e012541d438554494e14cce4b3e7c3c46fbb0
Mikus Lorencs <mikuslorence@gmail.com>
2013-01-21 20:16:44 -0700

    gyj

--- cmput414Lab1/src/gandalf.java
+++ cmput414Lab1/src/gandalf.java
@@ -22,15 +22,31 @@ public class gandalf {
 		
 		int minLightThreshold = 300;
 	    String state = "explore";
-	    int minSpeed = 50;
+	    int minSpeed = 60;
 	    
 	    int lightFound = 0;
 	    double totalHeading = 0;
-	    
+	    long timer = System.currentTimeMillis();
 		while (true) {
+			/*
+			if (Button.RIGHT.isPressed()){
+				light1.calibrateHigh();
+				light2.calibrateHigh();
+	        }
+			if (Button.LEFT.isPressed()){
+				light1.calibrateLow();
+				light2.calibrateLow();
+	        	break;
+	        }
+	        */
+			if (Button.ENTER.isPressed()){
+	        	break;
+	        }
 	        // draw to LCD
 	        LCD.drawInt(light1.getNormalizedLightValue(), 4, 0, 1);
 	        LCD.drawInt(light2.getNormalizedLightValue(), 4, 0, 2);
+
+	        //LCD.drawString(state, 0,3);
 	        //LCD.drawInt(light1.getLightValue(), 4, 0, 0);
 	        //LCD.drawInt(SensorPort.S1.readRawValue(), 4, 0, 2);
 	        //LCD.drawInt(SensorPort.S1.readValue(), 4, 0, 3);
@@ -39,9 +55,10 @@ public class gandalf {
 	        int lightRec1 = light1.getNormalizedLightValue();
 	        int lightRec2 = light2.getNormalizedLightValue();
 	        //System.out.println("state: " + state);
+	        /*
 	        if (state.equals("explore")){
-	    		MotorPort.A.controlMotor(60, BasicMotorPort.BACKWARD);
-	            MotorPort.C.controlMotor(60, BasicMotorPort.FORWARD);
+	    		MotorPort.A.controlMotor(75, BasicMotorPort.BACKWARD);
+	            MotorPort.C.controlMotor(75, BasicMotorPort.FORWARD);
 	        	
 	        	// save brightest light while exploring
 		        if (lightRec1 > lightFound){
@@ -66,21 +83,54 @@ public class gandalf {
 	        	}
 	        } else if (state.equals("searching")){
 		        
-	    		MotorPort.A.controlMotor(60, BasicMotorPort.BACKWARD);
-	            MotorPort.C.controlMotor(60, BasicMotorPort.FORWARD);
-	            
-	            if (lightFound < 300){
+	    		MotorPort.A.controlMotor(75, BasicMotorPort.BACKWARD);
+	            MotorPort.C.controlMotor(75, BasicMotorPort.FORWARD);
+	            */
+	            /*if (lightFound < 300){
 	            	state = "explore";
 	            	continue;
-	            }
-	            
-		        if ((Math.abs(lightRec1 - lightFound) == 50) || 
-		           (Math.abs(lightRec2 - lightFound) == 50)){
+	            }*/
+	       /*
+		        if ((Math.abs(lightRec1 - lightFound) == 20) || 
+		           (Math.abs(lightRec2 - lightFound) == 20)){
 		        	state = "moving";
 		        }
 	        } else if (state.equals("moving")){
-	        	int powerA = (lightRec1 - minLightThreshold) / (minSpeed);
-	        }
+	        	if ((lightRec1 < 300) && (lightRec2 < 300)){
+	        		state = "explore";
+	        		continue;
+	        	}
+	        */
+	        
+	        	if ((lightRec1 > 640) || (lightRec2 > 640)){
+		        	MotorPort.A.controlMotor(100, BasicMotorPort.STOP);
+		            MotorPort.C.controlMotor(100, BasicMotorPort.STOP);
+	        		continue;
+	        	}
+	        	double OldRange = (890 - minLightThreshold);  
+	        	double NewRange = (100 - minSpeed);
+	        	double powerA = (((lightRec1 - minLightThreshold) * NewRange) / OldRange) + minSpeed;
+	        	double powerC = (((lightRec2 - minLightThreshold) * NewRange) / OldRange) + minSpeed;
+	        	/*
+	        	// EXPLORATION MODE:
+	        	if (System.currentTimeMillis() - timer < 5000){
+	        		timer = System.currentTimeMillis();
+		        	if ((powerA < 60) && (powerC < 60)){
+		        		MotorPort.A.controlMotor(75, BasicMotorPort.BACKWARD);
+			            MotorPort.C.controlMotor(75, BasicMotorPort.FORWARD);
+			            continue;
+		            }
+	        	}
+	        	*/
+		        LCD.drawInt((int)powerA, 4, 0, 3);
+		        LCD.drawInt((int)powerC, 4, 0, 4);
+		        
+	        	MotorPort.A.controlMotor((int) powerA, BasicMotorPort.FORWARD);
+	            MotorPort.C.controlMotor((int) powerC, BasicMotorPort.FORWARD);
+	            
+
+	            
+	        //}
 	        
 
 	        
@@ -97,9 +147,7 @@ public class gandalf {
 	            MotorPort.C.controlMotor(70, BasicMotorPort.FORWARD);
 	        }
 	        */
-	        if (Button.ENTER.isPressed()){
-	        	break;
-	        }
+	        
 	     }
 	    
 		//start motors
@@ -107,61 +155,6 @@ public class gandalf {
 	    
 	    
 	    Button.waitForPress();
-		/*
-		int[][] command = {
-			      { 80, 60, 1},
-			      {-80, 80, 1},
-			      {	80, 80, 1}
-			    };		
-		
-		double totalHeading = 0;
-		double X = 0;
-		double Y = 0;
-		 
-		for (int i = 0; i < 3; i++){
-			int powerA = command[i][0];
-			int powerB = command[i][1];
-			int timeInterval = command[i][2];
-			
-			long intervalTimer = System.currentTimeMillis();
-			
-			//start motors
-			MotorPort.A.controlMotor(powerA, BasicMotorPort.FORWARD);
-	        MotorPort.C.controlMotor(powerB, BasicMotorPort.FORWARD);
-			
-	        MotorPort.A.resetTachoCount();
-	        MotorPort.C.resetTachoCount();
-	        
-			while(true){
-				int leftTick = MotorPort.A.getTachoCount();
-				int rightTick = MotorPort.C.getTachoCount();
-				MotorPort.A.resetTachoCount();
-		        MotorPort.C.resetTachoCount();
-		        
-				double deltaDistance = (leftTick + rightTick) / 2 * distPerTick;
-				double deltaHeading = ((rightTick - leftTick) * radiansPerTick) / 2;	
-				
-				totalHeading += deltaHeading;
-				
-				double deltaX = deltaDistance * Math.cos(totalHeading);
-				double deltaY = deltaDistance * Math.sin(totalHeading);
-				
-				X += deltaX;
-				Y += deltaY;
-				
-				if (System.currentTimeMillis() - intervalTimer > timeInterval*1000){
-					break;
-				}
-			}
-		}
-		
-		
-		MotorPort.A.controlMotor(100, BasicMotorPort.STOP);
-        MotorPort.C.controlMotor(100, BasicMotorPort.STOP);
-		   
-        System.out.println("Heading: " + Math.floor((totalHeading * 180/Math.PI)* 100) / 100);
-        */
-        //Button.waitForPress();
 
 	}
 
